f4ff05a8eb449850973f1c5584728f8e0f8c5aa8
Qapitolsaurabh <saurabh.s@qapitol.com>
2019-01-31 01:15:57 +0530

    First Commit

---
+++ spring-rest/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into employee
+values(10001,'Ranga', 'E1234567');
+
+insert into employee
+values(10002,'Ravi', 'A1234568');
+
+insert into employee
+values(10003,'Rahul', 'B123456');
---
+++ spring-rest/src/test/java/com/qapitol/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.qapitol.springrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
+
